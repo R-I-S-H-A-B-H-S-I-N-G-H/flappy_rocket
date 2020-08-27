19c6eeff440790ce3dd2f29059c62e2dae74a2ee
Rishabh Singh <35109053+R-I-S-H-A-B-H-S-I-N-G-H@users.noreply.github.com>
2020-08-27 17:35:40 +0530

    Update movement.cs

--- 4hourgame/Assets/movement.cs
+++ 4hourgame/Assets/movement.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-   // public Transform firepos;
     public GameObject fire;
     public float ypos;
     List<GameObject> l;
@@ -23,7 +22,7 @@ public class movement : MonoBehaviour
         if(Input.GetKeyDown("space")||Input.GetMouseButtonDown(0)){
             jump();
         }
-     //  transform.eulerAngles=new Vector3(0,0,rb.velocity.y*0.1f);
+     
     }
     void jump(){
         
@@ -36,26 +35,8 @@ public class movement : MonoBehaviour
         o= Instantiate(fire,fire.transform.position,Quaternion.identity);
        o.SetActive(true);
        l.Add(o);
-       //Destroy(o);
+
 
     }
-//     void destroy(){
-//         timer=0;
-//         if(l.Count>0){
-//             if(l.Count>=15){
-//                 for(int i=0;i<=10;i++){
-//                     Destroy( l[i]);
-//                     l.RemoveAt(i);
-
-//                 } 
-
-            
-//            else{
-//                 Destroy( l[0]);
-//                 l.RemoveAt(0);
-//            }
-//         }
-//     }
-// }
 }
 
